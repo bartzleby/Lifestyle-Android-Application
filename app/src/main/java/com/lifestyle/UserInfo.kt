30ed66a008fbbdfd766a153bd7360113c01a373a
Vanessa <vvannessafaye@gmail.com>
2023-03-16 22:40:58 -0600

    Added drop downs for all user info except City and Country

--- app/src/main/java/com/lifestyle/UserInfo.kt
+++ app/src/main/java/com/lifestyle/UserInfo.kt
@@ -10,11 +10,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import android.widget.Spinner
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import java.util.*
 
@@ -31,36 +31,52 @@ private const val ARG_PARAM2 = "param2"
 private var mThumbnailImage: Bitmap? = null
 
 //Define a global intent variable
-private var mDisplayIntent: Intent? = null
+//private var mDisplayIntent: Intent? = null
 
 
-class UserInfo : Fragment(),  View.OnClickListener {
+class UserInfo : Fragment(),  View.OnClickListener,  AdapterView.OnItemSelectedListener  {
     // TODO: Rename and change types of parameters
    // private var param1: String? = null
    //private var param2: String? = null
     //Create variables to hold the strings
     private var mFullName: String? = null
-    private var mAge: String? = null
+    private var mAge: Int? = null
     private var mCity: String? = null
     private var mCountry: String? = null
     private var mHeight: String? = null
-    private var mWeight: String? = null
+    private var mWeight: Int? = null
     private var mSex: String? = null
+    private var mActivity: String? = null
 
     private var mTvFullName: TextView? = null
-    private var mEtFullName: EditText? = null
-    private var mTvAge: TextView? = null
-    private var mTvCity: TextView? = null
-    private var mTvCountry: TextView? = null
-    private var mTvHeight: TextView? = null
-    private var mTvWeight: TextView? = null
-    private var mTvSex: TextView? = null
-    private var mTvActivity_level: TextView? = null
+    //private var mEtFullName: EditText? = null
+   // private var mTvAge: TextView? = null
+  //  private var mTvCity: TextView? = null
+   // private var mTvCountry: TextView? = null
+   // private var mTvHeight: TextView? = null
+   // private var mTvWeight: TextView? = null
+   // private var mTvSex: TextView? = null
+   // private var mTvActivityLevel: TextView? = null
     private var mButtonSubmit: Button? = null
     private var mButtonCamera: Button? = null
     private var mIvPic: ImageView? = null
+   // private var agePicker: NumberPicker? = null
+    private var sexSpinner: Spinner? = null
+    private var weightSpinner: Spinner? = null
+    private var activitySpinner: Spinner? = null
+    private var ageSpinner: Spinner? = null
+    private var feetSpinner: Spinner? = null
+    private var inchSpinner: Spinner? = null
+    //private var weightPicker: NumberPicker? = null
 
-    private var mDisplayIntent: Intent? = null
+   // private var mDisplayIntent: Intent? = null
+    private val sex_list : List<String> = listOf("Male", "Female")
+    private val activity_list : List<String> = listOf("Sedentary", "Lightly Active", "Moderately Active", "Very Active", "Extra Active")
+    private val  age_list = (13.. 100).toList()
+    private val weight_list = (85.. 500).toList()
+    private val feet_list : List<String> = listOf("1'", "2'", "3'", "4'", "5'", "6'", "7'")
+    private val inches_list : List<String> = listOf("0\"", "1\"", "2\"", "3\"", "4\"", "5\"", "6\"", "7\"", "8\"", "9\"", "10\"", "11\"")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,15 +84,6 @@ class UserInfo : Fragment(),  View.OnClickListener {
        //     param1 = it.getString(ARG_PARAM1)
      //       param2 = it.getString(ARG_PARAM2)
      //   }
-
-     /**   mButtonSubmit = findViewById(R.id.button_submit)
-        mButtonCamera = findViewById(R.id.button_take_pic)
-        mButtonSubmit!!.setOnClickListener(this)
-        mButtonCamera!!.setOnClickListener(this) */
-
-
-
-
 
     }
 
@@ -93,10 +100,115 @@ class UserInfo : Fragment(),  View.OnClickListener {
         mTvFullName = view.findViewById(R.id.name)
         mButtonSubmit!!.setOnClickListener(this)
         mButtonCamera!!.setOnClickListener(this)
+        // mTvSex = view.findViewById(R.id.sex_view)
+        //mTvActivityLevel = view.findViewById(R.id.activity_level_view)
 
-        return view;
+      /*  //age picker
+        agePicker = view.findViewById(R.id.number_picker_age)
+        agePicker!!.setMaxValue(100)
+        agePicker!!.setMinValue(10)
+        agePicker!!.setWrapSelectorWheel(true)
+        agePicker!!.setOnValueChangedListener(OnValueChangeListener { picker, oldVal, newVal ->
+            mAge = newVal
+
+        })*/
+
+  /*     //weight picker
+        weightPicker = view.findViewById(R.id.number_picker_weight)
+        weightPicker!!.setMaxValue(600)
+        weightPicker!!.setMinValue(95)
+        weightPicker!!.setWrapSelectorWheel(true)
+        weightPicker!!.setOnValueChangedListener(OnValueChangeListener { picker, oldVal, newVal ->
+            mWeight = newVal
+
+        }) */
+
+        //sex drop down
+        sexSpinner = view.findViewById(R.id.sex_spinner)
+        sexSpinner!!.setOnItemSelectedListener(this)
+        val ad = ArrayAdapter(view.context, android.R.layout.simple_spinner_item, sex_list)
+        ad.setDropDownViewResource(android.R.layout.select_dialog_item)
+        sexSpinner!!.setAdapter(ad)
+
+        //weight drop down
+        weightSpinner = view.findViewById(R.id.weight_spinner)
+        weightSpinner!!.setOnItemSelectedListener(this)
+        val ad2 = ArrayAdapter(view.context, android.R.layout.simple_spinner_item, weight_list)
+        ad.setDropDownViewResource(android.R.layout.select_dialog_item)
+        weightSpinner!!.setAdapter(ad2)
+
+        //activity drop down
+        activitySpinner = view.findViewById(R.id.activity_spinner)
+        activitySpinner!!.setOnItemSelectedListener(this)
+        val ad3 = ArrayAdapter(view.context, android.R.layout.simple_spinner_item, activity_list)
+        ad2.setDropDownViewResource(android.R.layout.select_dialog_item)
+        activitySpinner!!.setAdapter(ad3)
+
+        //age drop down
+        ageSpinner = view.findViewById(R.id.age_spinner)
+        ageSpinner!!.setOnItemSelectedListener(this)
+        val ad4 = ArrayAdapter(view.context, android.R.layout.simple_spinner_item, age_list)
+        ad2.setDropDownViewResource(android.R.layout.select_dialog_item)
+        ageSpinner!!.setAdapter(ad4)
+
+        //feet drop down
+        feetSpinner = view.findViewById(R.id.feet_spinner)
+        feetSpinner!!.setOnItemSelectedListener(this)
+        val ad5 = ArrayAdapter(view.context, android.R.layout.simple_spinner_item, feet_list)
+        ad2.setDropDownViewResource(android.R.layout.select_dialog_item)
+        feetSpinner!!.setAdapter(ad5)
+
+        //feet drop down
+        inchSpinner = view.findViewById(R.id.inches_spinner)
+        inchSpinner!!.setOnItemSelectedListener(this)
+        val ad6 = ArrayAdapter(view.context, android.R.layout.simple_spinner_item, inches_list)
+        ad2.setDropDownViewResource(android.R.layout.select_dialog_item)
+        inchSpinner!!.setAdapter(ad6)
+
+
+        return view
 
     }
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        //Toast.makeText(view!!.context, items[p2], Toast.LENGTH_SHORT).show();
+       when (p0!!.id) {
+            R.id.sex_spinner-> {
+              //  mTvSex!!.text = sex_list[p2]
+                mSex = sex_list[p2]
+            }
+            R.id.activity_spinner-> {
+               // mTvActivityLevel!!.text = activity_list[p2]
+                mActivity = activity_list[p2]
+
+            }
+           R.id.age_spinner-> {
+               // mTvActivityLevel!!.text = activity_list[p2]
+               mAge = age_list[p2]
+
+           }
+           R.id.weight_spinner-> {
+               // mTvActivityLevel!!.text = activity_list[p2]
+               mWeight= weight_list[p2]
+
+           }
+           R.id.feet_spinner-> {
+               // mTvActivityLevel!!.text = activity_list[p2]
+               mHeight= feet_list[p2]
+
+           }
+           R.id.inches_spinner-> {
+               // mTvActivityLevel!!.text = activity_list[p2]
+               mHeight = mHeight + inches_list[p2]
+
+           }
+            }
+
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        //empty
+    }
+
 
     override fun onClick(view: View) {
 
@@ -104,21 +216,19 @@ class UserInfo : Fragment(),  View.OnClickListener {
             R.id.button_submit -> {
 
                 mFullName = mTvFullName!!.text.toString()
-               if (mFullName.isNullOrBlank()) {
+                if (mFullName.isNullOrBlank()) {
                     //Complain that there's no text
                     Toast.makeText(activity, "Enter a name first!", Toast.LENGTH_SHORT)
                         .show()
-                } else {
-                   /*
-                   val BmrFragment = BmrFragment()
-                   activity!!.supportFragmentManager.beginTransaction().replace(R.id.user_frag_container, BmrFragment, "bmr_frag").addToBackStack(null).commit()
-                    */
-                   R.id.BmrFragment?.let {
-                       activity!!.supportFragmentManager.commit {
-                           findNavController().navigate(R.id.BmrFragment)
-                       }
-                   }
-               }
+                    Toast.makeText(activity, "this is your height " + mHeight, Toast.LENGTH_SHORT).show()
+                } else { //go to next fragment - bmr
+                    R.id.BmrFragment?.let {
+                        activity!!.supportFragmentManager.commit {
+                            findNavController().navigate(R.id.BmrFragment)
+                        }
+                    }
+                }
+
 
 
             }
@@ -154,6 +264,9 @@ class UserInfo : Fragment(),  View.OnClickListener {
             }
         }
 
+
+
+
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -176,5 +289,4 @@ class UserInfo : Fragment(),  View.OnClickListener {
 
 
 
-
-    }
+}
