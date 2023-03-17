8b880d976c6f2da5712675467e638cafd6fd34a0
tcadamson <taylor@catcat.dev>
2023-03-17 00:38:30 -0600

    Add tablet layout demo with navigation rail
    Required build changes:
    com.google.android.material:material:1.5.0 -> 1.7.0
    Add androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0

--- app/src/main/java/com/lifestyle/MainActivity.kt
+++ app/src/main/java/com/lifestyle/MainActivity.kt
@@ -22,44 +22,46 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     //private var mButtonRegister: Button? = null
 
+    private fun navigateToFragmentFromItem(itemId: Int): Boolean {
+        var fragmentId: Int? = null
+        when (itemId) {
+            R.id.item_bmr, R.id.action_bmr -> fragmentId = R.id.BmrFragment
+            R.id.item_user -> fragmentId = R.id.UserInfo
+        }
+        fragmentId?.let {
+            supportFragmentManager.commit {
+                findNavController(R.id.nav_host_fragment_content_main).navigate(fragmentId)
+            }
+        }
+        return fragmentId != null
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
-
-
-        binding.fabWeather.setOnClickListener { view ->
-            Snackbar.make(view, "Missing location info", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        binding.toolbar?.let {
+            appBarConfiguration = AppBarConfiguration(navController.graph)
+            setSupportActionBar(binding.toolbar)
+            setupActionBarWithNavController(navController, appBarConfiguration)
         }
 
-
-
-
-
-
-
-    }
-   /** override fun onClick(view: View) {
-        when (view.id) {
-            R.id.button_register -> {
-                val fragment = UserInfo.newInstance();
-                val transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id., fragment).commit();
-
-
+        binding.navigationRail?.let {
+//            binding.navigationRail!!.selectedItemId = R.id.item_bmr
+            binding.navigationRail!!.setOnItemSelectedListener { item ->
+                navigateToFragmentFromItem(item.itemId)
             }
         }
-    } */
 
+//        binding.fabWeather.setOnClickListener { view ->
+//            Snackbar.make(view, "Missing location info", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+//        }
+    }
 
     @SuppressLint("RestrictedApi")
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -76,17 +78,7 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        var fragmentId: Int? = null
-        when (item.itemId) {
-            R.id.action_bmr -> fragmentId = R.id.BmrFragment
-        }
-        fragmentId?.let {
-            supportFragmentManager.commit {
-                findNavController(R.id.nav_host_fragment_content_main).navigate(fragmentId)
-            }
-            return true
-        }
-        return super.onOptionsItemSelected(item)
+        return navigateToFragmentFromItem(item.itemId) or super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
