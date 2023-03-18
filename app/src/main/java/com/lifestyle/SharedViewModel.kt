b80b1485e5859256633d2104e56c501e005eb44c
tcadamson <taylor@catcat.dev>
2023-03-17 18:28:24 -0600

    Implement SavedStateHandle so that the ViewModel state is always preserved

--- app/src/main/java/com/lifestyle/SharedViewModel.kt
+++ app/src/main/java/com/lifestyle/SharedViewModel.kt
@@ -1,41 +1,45 @@
 package com.lifestyle
 
+import android.os.Parcelable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class SharedViewState(
     val gender: Int? = null,
     val age: Int? = null,
     val height: Int? = null,
     val weight: Int? = null,
     val activity: Int? = null
-)
+) : Parcelable
 
-class SharedViewModel : ViewModel() {
-    private val mutableState = MutableLiveData(SharedViewState())
-    val state: LiveData<SharedViewState> get() = mutableState
+class SharedViewModel(private val savedState: SavedStateHandle) : ViewModel() {
+    val state = savedState.getLiveData("state", SharedViewState())
 
     // Repetition is ugly, but the alternative (reflection) is even uglier ¯\_(ツ)_/¯
     // https://stackoverflow.com/questions/70498362/specify-field-to-copy-in-data-classes-kotlin
+    // Also see https://stackoverflow.com/questions/61166786/how-to-save-livedata-into-savestatehandle for SavedStateHandle info
 
     fun selectGender(index: Int) {
-        mutableState.value = state.value?.copy(gender = index)
+        savedState["state"] = state.value?.copy(gender = index)
     }
 
     fun selectAge(value: Int) {
-        mutableState.value = state.value?.copy(age = value)
+        savedState["state"] = state.value?.copy(age = value)
     }
 
     fun selectHeight(value: Int) {
-        mutableState.value = state.value?.copy(height = value)
+        savedState["state"] = state.value?.copy(height = value)
     }
 
     fun selectWeight(value: Int) {
-        mutableState.value = state.value?.copy(weight = value)
+        savedState["state"] = state.value?.copy(weight = value)
     }
 
     fun selectActivity(index: Int) {
-        mutableState.value = state.value?.copy(activity = index)
+        savedState["state"] = state.value?.copy(activity = index)
     }
 }
